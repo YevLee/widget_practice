75a92087ca31dee44f621d1611c53616bad4e9fe
jinzhanye <jinzhan.ye@foxmail.com>
2017-11-07 19:07:06 +0800

    chapter 8

---
+++ Alert/chapter8/js/main.js
@@ -0,0 +1,25 @@
+require.config({
+    paths: {
+        jquery: "../../lib/jquery",
+        window: "window",
+        jqueryUI: "../../lib/jquery-ui.min",
+    }
+});
+
+require(["jquery", "window"], function ($, w) {
+    new w.Window().alert({
+        title:"I am title",
+        content: "welcome!",
+        width: 300,
+        height: 150,
+        y: 50,
+        hasCloseBtn:true,
+        handler4CloseBtn: function () {
+            alert("you click close button");
+        },
+        handler4AlertBtn: function () {
+            alert("you click alert button");
+        },
+        skinClassName:"window_skin_a"
+    });
+});
